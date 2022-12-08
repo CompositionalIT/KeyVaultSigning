778a6ab93955f675acc9943e08068a580324a706
Tuomas Hietanen <thorium@iki.fi>
2022-12-08 17:59:40 +0000

    Use configured encoding

--- KeyVaultSignTests/Tests.fs
+++ KeyVaultSignTests/Tests.fs
@@ -63,13 +63,12 @@ type TestClass () =
 
         let msg = """{"Type":"FITestEvent","Version":1,"Payload":"test","Nonce":1583990063}"""
 
-        let msgBytes = msg |> Encoding.UTF8.GetBytes
         let signature = "vqOPgJiOAP6Bl9vIGK9mW6jj9HBt+4LGV79Bjf7djPNlGrpw6JS2lQnW/xw2BfUAXmiPcxi8kllK9NVAEDESum8kt78D+J5/HWX9jOWO4Tgu6LT27J6u6aWgnkIR6/rHtVJlzMeWiKLMp8hTmArM4cZWEg5tVfLS3OlZd2Fcvrj4lqDYj2oikZ6i8glym3EXqrURkj+1Z6M2ZLxs5MWU5gW8D3X653MyUBB5Zm5YxhUiAUhnKqripDKFcBS2weIWH6i02risc3UtDqqVb668qi7Ykwf6p+ZQFZUPZalVOdU46yWZMM1jI3jmAjnLTjuZ5MNg69F1U+JeRzJW04pQe28tK2ZjkUxdIasOVpzmJmtHoAvTdCbpppFcGTomWDu8acFx8ax+9qjqFnhE97vg/W/kWKlzDhSXbUev/x6MT12WYD7/z9iSZ18WoTaEUXWWu84g0q2nQke+iVi8tjJArLF4YlJgOuFqJMGscMIYOf/0KvCSh0R40ASsjp2EQKXJEXxGUvSRoq1ilKKHadxy9nY+iLbVdOapngz1dBpm7CkkjC4jgQMqI4jdf7uI+CqZRczjL/F4/onP3DO92oeXAFUDn9gF8Wqiqt4ZoH/UxAjrE+JdIfaxHt8pYJOVxiSjAUpDCHjdJsEEZkBXXl75aETUh/vZ2uh9RQV9i3GAEBA="//let signatureResult = KeyVault.sign this.keyvault this.certificateName msg
         let signBytes = signature |> System.Convert.FromBase64String
 
         let publicKeyXml = "<RSAKeyValue><Modulus>v71mKsJJhpfBPluwl2+1ZfGLNtE2EZWyf2UkwF/QGJddycsFoKVpKZZP+LLmrNLZXKJWd7k2tcj/jwKZEbIjpBOMzCTLmiTXNr8aBwgb7FhUX9AQ62jDKvRW7jUTFPkzDTOuLto02iDSUCLGSGpico1MM0uS0NgY9oy9pMZGISBulOXAZ/aFABqpzRsId+JGgHCCPJm/HF6uAp/rbF78VHnzA2GvNUrUXBm0vGiX/JPIc/xhItRpT7IcAM7/RAy6e7kKxak60FK7rQkXTrcXlD/u34644Tuip3Th+9IzALIUahijWJOnO5bSo5CG4jk/qke2m8egkj1ojDO4gxS54JWIdL1SpB6adFoyDYD5FNrnwMmRklSel/sb1hjHPkU+zex8t+i//meC8kOXPh/R65xbOXZlPIEqFz4+M6QSAGQCtAa5GRqiz2vAkcxHQHW07VLYRFUbRYlw4ju4w2PRM7ur+X0iMqdJiBQX6hMJIhiDMWXZvL3XwOooz7D4bk99vIliJ1mB821uER2oRV5FBJhdDq5VfAfXRrZwCrbo8HacTMw9NrN32vN9HGJi7bfm/y8FD9TQnsSV01dfMKayO3K1GbIx54bTy5wufv/n3kd4c2hkga9jRfa2HEFTSkLPkPoHLD8/NRs5j6a5Ua8/qXRJbFQIXhYAme9THhSiUcs=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>"
 
-        let res = KeyVault.verifyPublic publicKeyXml signBytes msgBytes
+        let res = KeyVault.verifyPublic publicKeyXml signBytes msg
         // res |> printfn "%A"
         Assert.IsNotNull(res);
         Assert.IsTrue res
